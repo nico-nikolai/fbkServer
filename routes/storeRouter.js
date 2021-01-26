97adfe353048bf3a5bda19616698ecfeb94d8ac4
Nick Rose <nickfbp@gmail.com>
2021-01-26 15:35:35 -0700

    Updated Store router and model to inclue reviews

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -72,4 +72,143 @@ storeRouter.route('/:itemId')
     .catch(err => next(err))
 });
 
+storeRouter.route('/:itemId/reviews')
+.get((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(storeItem.reviews);
+        } else {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem) {
+            storeItem.reviews.push(req.body);
+            storeItem.save()
+            .then(storeItem => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(storeItem.reviews);
+            }) 
+        } else {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /store/${req.params.itemId}/reviews`);
+})
+.delete((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem) {
+            for (let i = (storeItem.reviews.length-1); i >=0; i--) {
+                storeItem.reviews.id(storeItem.reviews[i]._id).remove();
+            }
+            storeItem.save()
+            .then(storeItem => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(storeItem.reviews);
+            })
+        } else {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
+storeRouter.route('/:itemId/reviews/:reviewId')
+.get((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem && storeItem.reviews.id(req.params.reviewId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(storeItem.reviews.id(req.params.reviewId));
+        } else if (!storeItem) {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Review ${req.params.reviewId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /store/${req.params.itemId}/comments/${req.params.reviewId}`);
+})
+.put((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem && storeItem.reviews.id(req.params.reviewId)) {
+            if (req.body.rating) {
+                storeItem.reviews.id(req.params.reviewId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                storeItem.reviews.id(req.params.reviewId).text = req.body.text;
+            }
+            storeItem.save()
+            .then(item => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(item);
+            })
+            .catch(err => next(err));
+        } else if (!storeItem) {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Review ${req.params.reviewId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Store.findById(req.params.itemId)
+    .then(storeItem => {
+        if (storeItem && storeItem.reviews.id(req.params.reviewId)) {
+            storeItem.reviews.id(req.params.reviewId).remove();
+            storeItem.save()
+            .then(item => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(item);
+            })
+            .catch(err => next(err));
+        } else if (!storeItem) {
+            err = new Error(`Store Item ${req.params.itemId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Review ${req.params.reviewId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
 module.exports = storeRouter;
